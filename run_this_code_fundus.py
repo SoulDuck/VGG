cb139a3785b6de4fb68d922385f50b542276d217
kimseongjung <plznw4me@naver.com>
2017-11-02 10:27:51 +0900

    decrease learning rate

--- run_this_code_fundus.py
+++ run_this_code_fundus.py
@@ -103,9 +103,9 @@ train_loss=1000.
 for step in range(max_iter):
     #### learning rate schcedule
     if step < 5000:
-        learning_rate = 0.01
-    elif step < 45000:
         learning_rate = 0.001
+    elif step < 45000:
+        learning_rate = 0.0001
     elif step  < 60000:
         learning_rate = 0.0001
     elif step < 120000:
