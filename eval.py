19ea49388a5bf55867c0450a51b848010a091864
kimseongjung <plznw4me@naver.com>
2017-11-16 09:58:33 +0900

    add ensemble mode

--- eval.py
+++ eval.py
@@ -53,6 +53,7 @@ def eval(model_path ,test_images):
     pred_ = sess.run(pred_ , feed_dict={x_ : test_images[:],is_training_:False})
     tf.reset_default_graph()
     print pred_[:1]
+    return pred_
 if __name__ =='__main__':
     model_path ='./models/step_118000_acc_0.838333427906/model'
     pred=eval(model_path, test_images)
