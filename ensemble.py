12272b366758d4d041679031fbf628b779c13630
kimseongjung <plznw4me@naver.com>
2017-11-16 09:48:56 +0900

    add ensemble mode

--- ensemble.py
+++ ensemble.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
     model_paths=get_models_paths('./models/vgg_13')
     print 'number of model paths : {}'.format(len(model_paths))
     train_images, train_labels, train_filenames, test_images, test_labels, test_filenames = fundus.type1(
-        './fundus_300_debug', resize=(299, 299))
+        './fundus_300', resize=(299, 299))
     acc, max_list=ensemble_with_all_combibation(model_paths ,test_images , test_labels)
     pred_sum=ensemble('./models', test_images )
     acc =eval.get_acc(pred_sum , test_labels)
