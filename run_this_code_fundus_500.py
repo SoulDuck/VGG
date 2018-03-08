172a4f48dfefd3f6fa69e19b2bd507e6fe77dd5e
kimseongjung <plznw4me@naver.com>
2018-03-09 00:41:37 +0900

    add ensemble overlay actmap

--- run_this_code_fundus_500.py
+++ run_this_code_fundus_500.py
@@ -318,7 +318,7 @@ for step in range(max_iter):
     """ #### training ### """
 
     train_fetches = [train_op, accuracy_op, loss_op]
-    batch_xs, batch_ys = input.next_batch(batch_size, train_imgs, train_labs  )
+    batch_xs, batch_ys , batch_fname= input.next_batch(batch_size, train_imgs, train_labs )
     train_feedDict = {x_: batch_xs, y_: batch_ys, lr_: learning_rate, is_training: True}
     _ , train_acc, train_loss = sess.run( fetches=train_fetches, feed_dict=train_feedDict )
     #print 'train acc : {} loss : {}'.format(train_acc, train_loss)
