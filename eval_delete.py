41fae9865cde6022225c359b4297123009e68c83
kimseongjung <plznw4me@naver.com>
2018-03-12 06:43:19 +0900

    add Post Processing kmeans and draw Contours

--- eval_delete.py
+++ eval_delete.py
@@ -346,6 +346,9 @@ if __name__ =='__main__':
         fig = plt.figure()
         ax = fig.add_subplot(111)
         img=Image.open(os.path.join(save_dir , name).replace('xy_',''))
+        if img.size[0] > 2000: # 이미지가 3000 , 2000 이면 아예 그래픽 카드에 안들어간다 . 그래서 이미지의 크기를 보전하면서 이미지를 줄인다
+            pct = 2000 / float(img.size[0])
+            ori_img=ori_img.resize( [int(img.size[0]*pct) , int(img.size[1]*pct)])
         ax.imshow(img)
         for rect in rects:
 
