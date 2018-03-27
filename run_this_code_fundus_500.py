9be887695374f28dc1a0648823e3343b0baa4f5a
seongjungkim <plznw4me@naver.com>
2018-03-27 13:39:53 +0900

    update

--- run_this_code_fundus_500.py
+++ run_this_code_fundus_500.py
@@ -149,7 +149,7 @@ if args.optimizer == 'adam':
 
 log_count =0;
 while True:
-    logs_root_path='./logs/fundus_500/{}'.format(args.folder_name )
+    logs_root_path='./logs/fundus_300_N_VS_R/{}'.format(args.folder_name )
     try:
         os.makedirs(logs_root_path)
     except Exception as e :
@@ -170,7 +170,7 @@ sess, saver , summary_writer =model.sess_start(logs_path)
 
 model_count =0;
 while True:
-    models_root_path='./models/fundus_500/{}'.format(args.folder_name)
+    models_root_path='./models/fundus_300_N_VS_R/{}'.format(args.folder_name)
     try:
         os.makedirs(models_root_path)
     except Exception as e:
