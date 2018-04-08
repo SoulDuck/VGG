6814070ab7cde2fb98ef9e2c2b9ffbc4a7904532
kimseongjung <plznw4me@naver.com>
2018-04-08 20:27:32 +0900

    add Post Processing kmeans and draw Contours

--- eval_fundus_calcium.py
+++ eval_fundus_calcium.py
@@ -1,5 +1,4 @@
 #-*- coding: utf-8 -*-
-
 """
     model_path = './models/vgg_11_Calc_N_VS_ABN_no_BN_AUG/0/best_acc/step_4000_acc_0.641666710377/model' #calcium score
     test_imgs=np.load('./Test_Data/calc_fundus/test_abnormal_img_300.npy')
@@ -25,7 +24,7 @@ import matplotlib as mpl
 import glob
 import copy
 import input
-
+import os
 import fundus
 import kmeans
 import draw_contour
@@ -181,6 +180,14 @@ def eval(model_path ,test_images , batch_size  , save_root_folder):
 
 
 def fn(model_path, strides,pool_indices,label):
+    """
+     VGG 모델을 복원하고 , Image 크기에 제약을 받지 않는 형태로 만듭니다
+    :param model_path:
+    :param strides:
+    :param pool_indices:
+    :param label:
+    :return:
+    """
     def _restore_WB(name):
         w = graph.get_tensor_by_name(name + '/kernel:0')
         b = graph.get_tensor_by_name(name + '/bias:0')
@@ -219,41 +226,31 @@ def fn(model_path, strides,pool_indices,label):
     conv_resize = tf.reshape(conv_resize, [-1, im_height * im_width, out_ch])
     classmap = tf.matmul(conv_resize, label_w, name='classmap')
     classmap = tf.reshape(classmap, [-1, im_height, im_width], name='classmap_reshape')
-
     saver=tf.train.Saver()
     saver.save(sess,'./models/vgg_11/retina_and_normal/model')
-
-
     return classmap ,sess ,x_
-
-
 
 if __name__ =='__main__':
 
     #test_images=np.reshape(test_images,[-1,299,299,3])
-    model_path = './models/vgg_11/step_9600_acc_0.700000047684/model' #calcium score
-    test_nor_imgs = np.load('./Test_Data/fundus300_0_10_300_inf/test_normal_img_300_0_10.npy')
-    test_abnor_imgs=np.load('./Test_Data/fundus300_0_10_300_inf/test_abnormal_img_300_300_inf.npy')
-    test_imgs=np.vstack([test_nor_imgs , test_abnor_imgs])
 
-    test_imgs=np.reshape(test_imgs , list(np.shape(test_imgs)) + [1,])
-    print np.shape(test_nor_imgs)
-    print np.shape(test_abnor_imgs)
-    print np.shape(test_imgs)
 
+    model_path = './models/vgg_11/step_13300_acc_0.66666674614/model' #calcium score
     # exam_id 별 결과를 얻는다
+    datadir='./Test_Data/ImgSize_300/nor_0_10_abnor_100_inf/1'
     for type in ['normal' , 'abnormal']:
-
-        f = open('./test_' + type + '_examId_with_imgs.pkl', 'rb')
+        f = open(os.path.join( datadir, 'test_' + type + '_examId_imgs.pkl'), 'rb')
         examIds_imgs = pickle.load(f)
         f.close()
 
-        f = open('./calc_result/' + type + '_result.csv', 'w')
+        # 결과를 기록합니다
+        f = open(os.path.join(datadir, 'calc_result/' + type + '_result.csv'), 'w')
         writer=csv.writer(f)
+
         # Normal
-        count =0
-        for key in examIds_imgs .keys()[:]:
-            print key
+        count =0 # global count , fundus 의 갯수를
+        for key in examIds_imgs.keys()[:]:
+
             imgs=np.asarray(examIds_imgs [key])
             imgs=imgs.reshape(list(np.shape(imgs)) + [1])
             rmn , exam_id =key.split('_')
@@ -261,91 +258,12 @@ if __name__ =='__main__':
             save_dir=os.path.join('./calc_result/'+type, rmn, exam_id)
             if not os.path.isdir(save_dir):
                 os.makedirs(save_dir)
-            preds=eval(model_path , imgs , batch_size=3 , save_root_folder=save_dir)
+            preds=eval(model_path , imgs , batch_size=60 , save_root_folder=save_dir)
             count += len(preds)
-            np.save(os.path.join(save_dir,'cal_preds.npy') , preds)
-
+            np.save(os.path.join(save_dir,'cal_preds.npy') , preds) # 이걸 왜 저장하지
+            mean_pred=np.sum(preds , axis=1)
             for p in preds:
-                writer.writerow([rmn , exam_id , p[0] ,p[1] ])
-        print '{} : {}'.format( type ,  count)
+                writer.writerow([rmn , exam_id , p[0] ,p[1] , mean_pred])
+        print '{} : {}'.format(type ,  count)
         f.close()
-
-
-
-
     exit()
-
-
-
-    save_dir = './calc_result/activation_maps/calc_fundus_300_ori'
-    classmap ,sess, x_ = fn( model_path, strides=[1, 1, 1, 1, 1, 1, 1, 1], pool_indices=[0, 1, 2, 3, 5, 7], label=1)
-
-    thres=0.5
-    limit=None
-    for  img in test_imgs[:limit]:
-
-        #ori_img=np.asarray(Image.open(path))
-        if ori_img.size[0] > 2000: # 이미지가 3000 , 2000 이면 아예 그래픽 카드에 안들어간다 . 그래서 이미지의 크기를 보전하면서 이미지를 줄인다
-            pct = 2000 / float(ori_img.size[0])
-            ori_img=ori_img.resize( [int(ori_img.size[0]*pct) , int(ori_img.size[1]*pct)])
-        ori_img=np.asarray(ori_img) #resize([2000,2000], Image.ANTIALIAS))
-        img=ori_img.reshape((1,)+np.shape(ori_img))
-        img_h, img_w = ori_img.shape[:2]
-        actmap = sess.run(classmap, feed_dict={x_: img/255.})
-        actmap = np.squeeze(actmap)
-        actmap = np.asarray((map(lambda x: (x - x.min()) / (x.max() - x.min()), actmap)))  # -->why need this?
-        overlay = cam.overlay(actmap, ori_img, save_path='tmp_overlay.png', factor=0.1)
-        cmap=reverse_colourmap(plt.cm.jet) # reverse actmap Red -> Green - > Blue --> Blue --> Green --> Red
-        actmap=cmap(actmap)
-        plt.imsave(fname='tmp.png',arr=actmap)
-        actmap=Image.open('tmp.png').convert('RGB')
-        os.remove('tmp.png')
-        actmap=np.asarray(actmap)
-        #plt.imshow(actmap, cmap=plt.cm.jet, alpha=0.5, interpolation='nearest', vmin=0, vmax=1)
-        actmap=copy.copy(actmap)
-
-        actmap[:, :, 0] = np.zeros(np.shape(actmap)[:2]) # Red 을 지운다
-        actmap[:, :, 1] = np.zeros(np.shape(actmap)[:2]) # Green 을 지운다
-        plt.imsave(arr=actmap , fname =os.path.join(save_dir,'{}_actmap.png'.format(name)))
-        plt.show()
-        plt.imsave(arr=ori_img, fname =os.path.join(save_dir,'{}_ori.png'.format(name)))
-        plt.show()
-
-        # Mask
-        flatted_actmap_b=actmap[:, :, 2].reshape(-1) #
-        actmap_b_indices=np.where([flatted_actmap_b > 50])[1] # 왜 50 이상만 뽑는거지? rgb
-        #flatted_actmap_g = actmap[:, :, 1].reshape(-1)
-        #actmap_g_indices = np.where([flatted_actmap_g > 50])[1]
-
-        # indices_rg 의 목표는 actmap과 혼합된 이미지를 보존하는 것이다 # rg = Red Green 을 뜻한다.
-        indices_b = np.hstack([actmap_b_indices]) # 만약 초록색을 추가하기 원한다면 actmap_g_indices 이걸 추가하면 된다.
-        indices_b = np.asarray(list(set(indices_b)))
-
-        # Get original image pixels from indices_b
-        flatted_ori_img=ori_img.reshape([-1,3])
-        flatted_ori_img=flatted_ori_img.copy()
-        flatted_ori_img[indices_b]=np.array([0,0,0]) #
-
-        # Save Masked original image
-        masked_ori_img = flatted_ori_img.reshape([img_h, img_w, 3])
-
-        # Get rev_indices_rg
-        rev_indices_b=set(range(img_h*img_w))
-        rev_indices_b=rev_indices_b.difference(indices_b)
-        assert img_h*img_w==len(rev_indices_b) + len(indices_b)
-
-        # For getting rid of margin , extract indices
-        grey_ori_img=np.sum(ori_img , axis=2)
-        flatted_grey_ori_img=grey_ori_img.reshape(-1)
-        maring_indices=set(np.where([flatted_grey_ori_img< 10])[1])
-        rev_indices_b = list( maring_indices | rev_indices_b )
-
-        # Get Part of actmap from rev_indices_rg
-        flatted_overlay = overlay.reshape([-1, 3])
-        flatted_overlay= flatted_overlay.copy()
-        flatted_overlay[rev_indices_b] = np.array([0, 0, 0])  ##중요
-
-        # Save Masked Actmap image
-        masked_actmap=flatted_overlay.reshape([img_h,img_w,3])
-        blended_image = masked_actmap + masked_ori_img
-        plt.imsave(arr=blended_image, fname =os.path.join(save_dir,'{}_blend.png'.format(name)))
