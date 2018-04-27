07d038cc6a851b0d269e91a1459d1fcb0888da36
kimseongjung <plznw4me@naver.com>
2018-04-27 18:33:52 +0900

    code for seoulfundus-calcium classifer

--- aug.py
+++ aug.py
@@ -53,7 +53,7 @@ def aug_lv0(image_ , is_training , image_size):
     def aug_with_train(image, image_size):
 
         image = tf.image.resize_image_with_crop_or_pad(image, image_size+4, image_size+4)
-        image = tf.random_crop(image, [image_size, image_size, 3])
+        image = tf.random_crop(image, [image_size, image_size, 1])
         image = tf.image.random_flip_left_right(image)
         image = tf.image.random_flip_up_down(image)
 
@@ -61,13 +61,13 @@ def aug_lv0(image_ , is_training , image_size):
 
         #image = tf.image.random_brightness(image, max_delta=63. / 255.)
         #image = tf.image.random_saturation(image, lower=0.5, upper=1.8)
-        #image = tf.image.per_image_standardization(image)
+        image = tf.image.per_image_standardization(image)
         return image
 
     def aug_with_test(image , image_size):
 
         image = tf.image.resize_image_with_crop_or_pad(image, image_size, image_size)
-        #image = tf.image.per_image_standardization(image)
+        image = tf.image.per_image_standardization(image)
         return image
 
     image=tf.cond(is_training , lambda : aug_with_train(image_ , image_size=image_size)  , \
