1f5e742605c18f153233b6191caa5c9983ce3683
kimseongjung <plznw4me@naver.com>
2018-04-27 17:16:24 +0900

    code for seoulfundus-calcium classifer

--- run_this_code_CACSSEOUL_CACS.py
+++ run_this_code_CACSSEOUL_CACS.py
@@ -188,10 +188,10 @@ if args.use_clahe:
 train_nor_imgs=np.vstack([seoul_train_nor_imgs ,train_normal_imgs ])
 train_abnor_imgs=np.vstack([seoul_train_abnor_imgs ,train_abnormal_imgs ])
 
-train_imgs=np.vstack([train_nor_imgs , train_abnormal_imgs])
+train_imgs=np.vstack([train_nor_imgs , train_abnormal_imgs , train_abnormal_imgs,train_abnormal_imgs,train_abnormal_imgs])
 print '# Normal Training Images shape {} ',np.shape(train_nor_imgs)
 print '# ABNormal Training Images shape {} ',np.shape(train_abnor_imgs)
-print '# Training Data shape {} ',np.shape(train_imgs)
+print '# Training Data shape {} ',np.shape(train_abnor_imgs)
 
 exit()
 train_labs=cls2onehot(train_labs , 2)
