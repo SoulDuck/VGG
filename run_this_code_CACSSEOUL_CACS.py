71faab445e68becf7202a55ad43d4d0df4aff8be
kimseongjung <plznw4me@naver.com>
2018-04-27 17:54:18 +0900

    code for seoulfundus-calcium classifer

--- run_this_code_CACSSEOUL_CACS.py
+++ run_this_code_CACSSEOUL_CACS.py
@@ -128,6 +128,8 @@ seoul_train_nor_imgs , seoul_train_nor_labs , seoul_train_nor_fnames = reconstru
 seoul_train_abnor_imgs , seoul_train_abnor_labs , seoul_train_abnor_fnames = reconstruct_tfrecord_rawdata(os.path.join(root_dir , 'abnormal_test.tfrecord') , ch=1)
 seoul_test_imgs , seoul_test_labs , seoul_test_fnames = reconstruct_tfrecord_rawdata(os.path.join(root_dir , 'test.tfrecord'),ch=1)
 
+seoul_train_nor_labs =cls2onehot(seoul_train_nor_labs , 2)
+seoul_train_abnor_labs =cls2onehot(seoul_train_abnor_labs , 2)
 seoul_test_labs =cls2onehot(seoul_test_labs , 2)
 print '# Seoul Normal Training Images : {}'.format(np.shape(seoul_train_nor_imgs))
 print '# Seoul ABNormal Training Images : {}'.format(np.shape(seoul_train_abnor_imgs))
