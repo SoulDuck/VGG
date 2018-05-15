8c10e58ce561f7cab276815d559684dcb5325f16
seongjungkim <plznw4me@naver.com>
2018-05-15 15:26:03 +0900

    add count_trainable_params()

--- cam.py
+++ cam.py
@@ -136,7 +136,8 @@ def eval_inspect_cam(sess, cam ,cam_ind, top_conv ,test_imgs , x, y_ ,phase_trai
         cam_vis_abnormal=cmap(cam_vis_abnormal)
         plt.imsave('{}/abnormal_actmap.png'.format(save_dir), cam_vis_abnormal)
         plt.imsave('{}/normal_actmap.png'.format(save_dir), cam_vis_normal)
-        plt.imsave('{}/abnormal_actmap_1.png'.format(save_dir), cam_vis_abnormal_1)
+        plt.show(cam_vis_abnormal_1)
+        #plt.show()('{}/abnormal_actmap_1.png'.format(save_dir), cam_vis_abnormal_1)
 
         """
         print np.shape(cam_vis_abnormal)
